01bff77ce24cca05c5c8eda6aa44335d6420eb82
Stuart Alexander <stuartalexander2@gmail.com>
2020-07-18 11:00:33 +0100

    Proposed Submission

--- js/chart.js
+++ js/chart.js
@@ -6,13 +6,10 @@ let trafficNavList = document.getElementsByClassName("traffic-nav")[0];
 
 let hourLabels = ["2-4", "4-6","6-8","8-10","10-12","10-12","12-14","14-16","16-18","18-20","20-22","22-24"];
 let hourData = [ 750 , 1250 , 1000 , 2000 , 1500 , 1750 , 1250 , 1850 , 2250 , 1500 , 2500, 2000];
-
 let dayLabels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 let dayData = [650, 2300, 1000, 2000, 1250, 2750, 2000];
-
 let weekLabels = ["Week-1","Week-2","Week-3","Week-4","Week-5","Week-6","Week-7","Week-8","Week-9","Week-10","Week-11","Week-12",];
 let weekData = [ 850 , 1350 , 900 , 1900 , 1400 , 1850 , 1350 , 1650 , 2750 , 1000 , 2200, 2200];
-
 let monthLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 let monthData = [ 650 , 1550 , 700 , 2100 , 1200 , 2050 , 1550 , 1850 , 2950 , 2000 , 2600, 2100];
 
@@ -43,43 +40,33 @@ let trafficChart = new Chart(trafficCanvas, {
 
 });
 
-function classRemove(){
-const nav = document.querySelector(".traffic-nav")
-for (let i = 0; i < nav.length; i++)
-nav[i].classList.remove("selected")
+function chartUpdate(){
+    trafficChart.update();
+    trafficNavList.classList.toggle("selected");
+
 }
 
 trafficNavList.addEventListener("click", (event) => {
-
+    trafficNavList.classList.remove("selected")
     trafficNavList = event.target
-       classRemove();
        if(trafficNavList.innerHTML == "Hourly"){
         trafficData.labels = hourLabels
         trafficData.datasets[0].data = hourData
-        trafficChart.update();
-        trafficNavList.classList.toggle("selected")
+        chartUpdate()
     } else if (trafficNavList.innerHTML == "Daily"){
         trafficData.labels = dayLabels
         trafficData.datasets[0].data = dayData
-        trafficChart.update();
-        trafficNavList.classList.toggle("selected")
+        chartUpdate()
     } else if (trafficNavList.innerHTML == "Weekly"){
         trafficData.labels = weekLabels
         trafficData.datasets[0].data = weekData
-        trafficChart.update();
-        trafficNavList.classList.toggle("selected")
+        chartUpdate()
     } else if (trafficNavList.innerHTML == "Monthly"){
         trafficData.labels = monthLabels
         trafficData.datasets[0].data = monthData
-        trafficChart.update();
-        trafficNavList.classList.toggle("selected")
+        chartUpdate()
     }
 })
-
-
-
-
-
 
 
 //end of traffic line graph
--- js/main.js
+++ js/main.js
@@ -1,9 +1,65 @@
-const alertBanner = document.getElementById("alert");   
+//JS to create menu for alert icon notifications
+
+const badge = document.querySelector(".badge");
+const bellContainer = document.querySelector(".bell-container");
+let alertItemsMenu = ["There has been an increase in Traffic to the website", "Mobile stats have declined", "A new member message has been recieved"];
+let newList = document.createElement("ul");
+const bellIcon = document.querySelector(".bell-icon .bell-detail");
+const closeAlert = document.querySelector(".close");
+
+badge.innerHTML = alertItemsMenu.length;
+
+if (alertItemsMenu.length > 0){
+    bellIcon.style.fill = "#78CF82";
+    bellIcon.style.transition = "ease-in 2s";
+}
+
+bellContainer.addEventListener("click", (event) => {
+    if (event.target.classList == "badge" || "bellContainer")
+    badge.style.display = "none";
+    if (alertItemsMenu.length > 0 ){   
+        newList.classList.add("alert-list");
+        for (let i = 0; i < alertItemsMenu.length; i++){
+            bellContainer.appendChild(newList);
+            let listItems = document.createElement("li");
+            let closeButton = document.createElement("button");
+            closeButton.classList.add("close");
+            closeButton.innerHTML = "Close"
+            listItems.innerHTML = alertItemsMenu[i]; //This inputs the notifications stored in the alert mennu array as HTML
+            newList.appendChild(listItems); // creates new list unordered list with new list items
+            listItems.appendChild(closeButton);                
+        }
+        function empty (){
+            alertItemsMenu = [];
+        }
+        empty()
+    }
+});
+
+//This scope hides the list item once the client clicks on the close button
+
+bellContainer.addEventListener("click", (event) => {
+    if (event.target.classList == "close"){
+        let li = document.querySelector("li")[0]; 
+        const close = event.target;
+        li = close.parentNode;
+        li.style.display = "none";
+        if (li.length == null){  
+        bellIcon.style.fill = "white"
+        }
+    }
+
+})
+
+//End of setting up the bell notificaion and list itemss.
+
+
 
 // Alert Banner for main page that displays overdue tasks
-
+const alertBanner = document.getElementById("alert");   
 const alertBannerHTML = document.createElement("p");
 const closeBanner = document.createElement("p");
+
 alertBannerHTML.innerHTML = ("<strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete");
 alertBannerHTML.classList.add("alert-banner");
 closeBanner.innerHTML = ("x");
@@ -22,14 +78,13 @@ alertBanner.addEventListener("click", (event) => {
 
 
 
-//messaging section
+//messaging section to ensure that the form is completed successfully
 
 const user = document .getElementById( "userField" );
 const message = document .getElementById( "messageField" );
 const send = document .getElementById( "send" );
 
 send.addEventListener( 'click' , () => {
-    // ensure user and message fields are filled out
     if (user.value === "" && message.value === "" ) {
     alert( "Please fill out user and message fields before sending" );
     } else if (user.value === "" ) {
@@ -40,77 +95,77 @@ send.addEventListener( 'click' , () => {
     alert( `Message successfully sent to: ${user.value}` );
     }
     });
-  
-
-
-//JS to create menu for alert icon
-const badge = document.querySelector(".badge");
-const bellContainer = document.querySelector(".bell-container");
-let alertItemsMenu = ["There has been an increase in Traffic to the website", "Mobile stats have declined", "A new member message has been recieved"];
-let newList = document.createElement("ul");
-const bellIcon = document.querySelector(".bell-icon .bell-detail")
-
-badge.innerHTML = alertItemsMenu.length
-if (alertItemsMenu.length > 0){
-bellIcon.style.fill = "green";
-bellIcon.style.transition = "ease-in 1s";
-
-}
-
-bellContainer.addEventListener("click", (event) => {
-    if (event.target.classList == "badge" || "bellContainer")
-    badge.style.display = "none";
-    if (alertItemsMenu.length > 0 ){   
-        newList.classList.add("alert-list");
-        for (let i = 0; i < alertItemsMenu.length; i++){
-            bellContainer.appendChild(newList);
-            let listItems = document.createElement("li");
-            let closeButton = document.createElement("button");
-            closeButton.classList.add("close");
-            closeButton.innerHTML = "Close"
-            listItems.innerHTML = alertItemsMenu[i];
-            newList.appendChild(listItems); 
-            listItems.appendChild(closeButton);                
-        }
-        function empty (){
-            alertItemsMenu = [];
-        }
-        empty()
-    }
-});
-
-const closeAlert = document.querySelector(".close");
-
-bellContainer.addEventListener("click", (event) => {
-
-    if (event.target.classList == "close"){
-        let li = document.querySelector("li")[0];
-        const close = event.target;
-        li = close.parentNode;
-        li.style.display = "none";
-        if (li.length == null){
-        bellIcon.style.fill = "white"
-        }
-    }
-
-})
 
   //This is the JS to support filter on search box
 
-let members = ["Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver"];
-let search = document.getElementById("search-list");
+  let members = ["Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver"];
+  let search = document.getElementById("search-list");  
+  
+  for (let i = 0; i < members.length; i++){
+      let option = document.createElement("option");
+      option.value = members[i];
+      search.appendChild(option)
+  }    
 
 
-for (let i = 0; i < members.length; i++){
-    let option = document.createElement("option");
-    option.value = members[i];
-    search.appendChild(option)
+
+
+
+
+
+
+//This is JS to store setting items in the local storage
+
+let emailNotifications = document.getElementById("email-notifications");
+let profileSettings = document.getElementById("profile-settings");
+let timezone = document.getElementById("timezone");
+let saveBtn = document.getElementById("save");
+let cancelBtn = document.getElementById("cancel");
+let emailPref = localStorage.getItem('emailPref');
+let profilePref = localStorage.getItem('profilePref');
+let timeZonePref = localStorage.getItem('timezonePref');
+
+function testStorage() {
+    const test = 'test';
+      try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+      } catch(e) {
+        return false;
+    }
+   }
+
+const loadSettings = function() {
+    if (emailPref !== null) {
+    emailNotifications.checked =  (emailPref === "true")
+    }
+    if (profilePref !== null) {
+    profileSettings.checked = (profilePref === "true")
+    }
+    timezone.value = localStorage.getItem("timeZonePref");
 }
 
 
+if(testStorage() === true){ 
+    document.getElementById("save").addEventListener("click", function(){
+        localStorage.setItem('emailPref', emailNotifications.checked);
+        localStorage.setItem('profilePref', profileSettings.checked);
+        localStorage.setItem('timeZonePref', timezone.value);        
+        alert('Settings successfully saved!');
 
+    });
+    console.log(timeZonePref)
+    document.getElementById("cancel").addEventListener("click", function() {
+        const cancel = confirm('Are you sure you want to cancel changes?');
+        if (cancel) {
+            localStorage.setItem('emailPref', emailNotifications.checked = null)
+            localStorage.setItem('profilePref', profileSettings.checked = null)
+            localStorage.setItem('timeZonePref', timezone.value = "Select a Timezone")
 
+        }
+      });
+      loadSettings();
+    }
 
-
-
-
+    
